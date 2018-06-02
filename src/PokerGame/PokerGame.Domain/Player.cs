1df45ea0b721caceaec2e4a0d8eac3f02da808be
GillesDV <gilles.devylder@hotmail.com>
2018-06-02 23:30:54 +0200

    Add Player and PokerTable classes

---
+++ src/PokerGame/PokerGame.Domain/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGame.Domain
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Card> Cards { get; set; }
+
+        public Player(string name)
+        {
+            Cards = new List<Card>();
+            Name = name;
+        }
+    }
+}
---
+++ src/PokerGame/PokerGame.Domain/PokerTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGame.Domain
+{
+    public class PokerTable
+    {
+        public List<Player> Players { get; set; }
+
+        public PokerTable(List<Player> players)
+        {
+            Players = players;
+        }
+    }
+}
